0186574c3fbd95b2de43add1e296cd353b4b3ba1
Andrew Schwimmer <andrew.schwimmer@disneystreaming.com>
2024-05-02 15:00:18 -0400

    Added ApiKey class

---
+++ src/main/java/andrewoid/EnvironmentHandler.java
@@ -0,0 +1,17 @@
+package andrewoid;
+
+import org.jetbrains.annotations.Nullable;
+
+public class EnvironmentHandler implements Handler {
+
+    private final String keyName;
+
+    public EnvironmentHandler(String keyName) {
+        this.keyName = keyName;
+    }
+
+    @Override
+    public @Nullable String locate() {
+        return System.getenv(keyName);
+    }
+}
---
+++ src/main/java/andrewoid/Handler.java
@@ -0,0 +1,9 @@
+package andrewoid;
+
+import org.jetbrains.annotations.Nullable;
+
+public interface Handler {
+
+    @Nullable String locate();
+
+}
